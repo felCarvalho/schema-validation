f1209c6d0b9d6f6ac590a499b4e4530dcedac6fd
felipe <felipecarvalho2934@gmail.com>
2026-06-23 23:05:29 -0300

    feat: adiciona filtro groups/pick/omit no execute e atualiza docs

--- src/types.ts
+++ src/types.ts
@@ -11,9 +11,16 @@ export interface ResultPattern<T> {
     data: T;
 }
 
+export interface OptionsCommand {
+    groups?: string[];
+    pick?: string[];
+    omit?: string[];
+}
+
 export interface Rule<T, C> {
     key: keyof T;
     error: (data: T, context: C) => string;
+    groups?: string[];
     transform?: (data: T, context: C) => T | Promise<T>;
     condition?: (data: T, context: C) => boolean | Promise<boolean>;
     runValidate(data: T, context: C): boolean | Promise<boolean>;
@@ -21,5 +28,9 @@ export interface Rule<T, C> {
 }
 
 export interface Command<T extends object, R extends object, C extends object> {
-    execute(data: T, context: C): Promise<R>;
+    execute(
+        data: T,
+        context: C,
+        { groups, pick, omit }: OptionsCommand,
+    ): Promise<R>;
 }
